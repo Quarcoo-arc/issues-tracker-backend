d3e45e4fd5752627d76644bcd6ed9dab7831d679
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-09-02 23:34:14 +0000

    Add issue model

---
+++ src/models/index.js
@@ -0,0 +1,5 @@
+const Issue = require("./issue");
+
+module.exports = {
+  Issue,
+};
---
+++ src/models/issue.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const issueSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Issue = mongoose.model("Issue", issueSchema);
+
+module.exports = Issue;
